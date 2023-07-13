b92c4dc4715ccd1bfe525030432e1607cbdd268e
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-13 11:39:28 +0530

    modify

--- main.tf
+++ main.tf
@@ -68,6 +68,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   }
   computer_name                   = "myvm"
   admin_username                  = "azureuser"
+  admin_password                  = "password@768954"
   disable_password_authentication = false
 }
 
