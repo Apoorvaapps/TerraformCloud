580351d55f7f4fc3ee12a886a0dc868118dcaf03
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-13 10:35:31 +0530

    added

--- variables.tf
+++ variables.tf
@@ -14,5 +14,5 @@ variable "admin_password" {
   type        = string
   description = "The administrator password of the SQL logical server."
   sensitive   = true
-  default     = null
+  default     = "password"
 }
