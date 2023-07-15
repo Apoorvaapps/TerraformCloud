9d29558ba1b5c0a5920a1eb4ae1258828e4358fd
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-15 20:04:30 +0530

    modify

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "instance_count" {
   description = "Number of instances to provision."
   type        = number
-  default     = 2
+  #default     = 2
 }
 
 variable "admin_username" {
   type        = string
   description = "The administrator username of the SQL logical server."
-  default     = "azureadmin"
+  #default     = "azureadmin"
 }
 
 variable "admin_password" {
   type        = string
   description = "The administrator password of the SQL logical server."
   sensitive   = true
-  default     = "password@123"
+  #default     = "password@123"
 }
